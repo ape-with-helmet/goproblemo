f1de31ff27b4ad91b23a3bac0f7118eedf3904d8
ape-with-helmet <133648612+ape-with-helmet@users.noreply.github.com>
2023-06-10 15:32:41 +0530

    Update factorials.go

--- factorials.go
+++ factorials.go
@@ -1,16 +1,13 @@
 package main
 
 import "fmt"
-
-func fact(a int) {
-	if a == 0 {
-		return 1
-	}
-	return a * fact(a-1)
-}
 func main() {
 	fmt.Println("Enter a few numbers")
-	var n int
-	fmt.Scan(&n)
-	fmt.Print(fact(n))
+	var a int
+	fact:=1
+	fmt.Scan(&a)
+	for i:=a;i>0;i--{
+	    fact=fact*i
+	}
+	fmt.Print(fact)
 }
