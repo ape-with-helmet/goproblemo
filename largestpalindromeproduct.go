f65d4791d02980a99bbf8d123c3769c0dfa02a0c
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2023-06-21 09:52:14 +0000

    finaledit

--- largestpalindromeproduct.go
+++ largestpalindromeproduct.go
@@ -3,12 +3,39 @@ import (
 	"fmt"
 	"strconv"
 )
+var n int
 func ispalindrome(n int) bool {
 	str:=strconv.Itoa(n)
 	length:= len(str)
 	for i := 0; i < length/2; i++ {
-		if str[i] != str[length+1]{
+		if str[i] != str[length-i-1]{
 			return false
 		}
 	}
+	return true
+}
+func largestpalindromeproduct(n int) (int,int,int){
+	largestPalindrome:=0
+	var multiplicant1, multiplicant2 int
+	for i := n; i >= 1; i-- {
+		for j := i; j >= 1; j-- {
+			product:=i*j
+			if product<largestPalindrome{
+				break
+			}
+			if ispalindrome(product)&&product>largestPalindrome{
+				largestPalindrome=product
+				multiplicant1=i
+				multiplicant2=j
+			}
+		}
+	}
+	return largestPalindrome,multiplicant1,multiplicant2
+}
+func main(){
+	fmt.Println("Enter the limits: ")
+	fmt.Scan(&n)
+	result,multiplicant1,multiplicant2:=largestpalindromeproduct(n)
+	fmt.Println("The largest palindrome product is: ",result)
+	fmt.Printf("The multiplicands are: %d and %d",multiplicant1,multiplicant2)
 }
