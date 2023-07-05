4aa2389ee3b5d4b55bfabc938ee2a93711fce812
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2023-07-05 10:34:41 +0000

    hmm

--- httpreq.go
+++ httpreq.go
@@ -14,9 +14,25 @@ type Post struct{
 }
 func main(){
 	url:="https://jsonplaceholder.typicode.com/posts/1"
-	response.err:=http.Get(url)
+	response,err:=http.Get(url)
 	if err!=nil{
-		fmt.printf("HTTTP GET request failed: %s\n",err);
-		
+		fmt.Printf("HTTTP GET request failed: %s\n",err)
+		return
 	}
+	defer response.Body.Close()
+	body,err:=ioutil.ReadAll(response.Body)
+	if err!=nil{
+		fmt.Printf("Failed to read response body: %s\n",err)
+		return
+	}
+	var post Post
+	err=json.Unmarshal(body,&post)
+	if err!=nil{
+		fmt.Printf("Failed to parse JSON: %s\n",err)
+		return
+	}
+	fmt.Printf("UserID: %d\n",post.UserID);
+	fmt.Printf("ID: %d\n",post.ID);
+	fmt.Printf("Title: %s\n",post.Title);
+	fmt.Printf("Body: %s\n",post.Body);
 }
--- professor.go
+++ professor.go
@@ -1,78 +1,70 @@
 package main
 
-import(
+import (
 	"fmt"
 	"sync"
 	"time"
 )
-const(
-	numphilosophers=3
-	maxDiningCycles=2
+
+const (
+	numPhilosophers = 3
+	maxDiningCycles = 2
 )
-type Philosopher struct{
-	id int 
-	leftfork,rightfork *sync.Mutex
-	diningCycles int
+
+type Philosopher struct {
+	id                  int
+	leftFork, rightFork *sync.Mutex
+	diningCycles        int
 }
-type DiningTable struct{
-	philosophers []*Philosopher
-	waiter *sync.Mutex
+type DiningTable struct {
+	philososphers []*Philosopher
+	waiter        *sync.Mutex
 }
-func (p*Philosopher)think(){
-	fmt.Printf("Philosopher %d is thinking\n",p.id);
+
+func (p *Philosopher) think() {
+	fmt.Printf("Philosopher %d is thinking\n", p.id)
 	time.Sleep(time.Second)
 }
-func (p*Philosopher)eat(){
-	p.leftfork.Lock()
-	p.rightfork.Lock()
-	fmt.Printf("Philosopher %d is eating\n",p.id);
+func (p *Philosopher) eat() {
+	p.leftFork.Lock()
+	p.rightFork.Lock()
+	fmt.Printf("Philosopher %d is eating\n", p.id)
 	time.Sleep(time.Second)
-	p.leftfork.Unlock()
-	p.rightfork.Unlock()
+	p.rightFork.Unlock()
+	p.leftFork.Unlock()
 	p.diningCycles++
 }
-func (p*Philosopher) dine (table*DiningTable) {
-	fmt.Printf("!");
-	for p.diningCycles<maxDiningCycles{
+func (p *Philosopher) dine(table *DiningTable) {
+	for p.diningCycles < maxDiningCycles {
 		p.think()
 		table.waiter.Lock()
 		p.eat()
 		table.waiter.Unlock()
 	}
 }
-func main(){
-	fmt.Printf("1");
-	table:=&DiningTable{
-		philosophers: make([]*Philosopher, numphilosophers),
-		waiter: &sync.Mutex{},
+func main() {
+	table := &DiningTable{
+		philososphers: make([]*Philosopher, numPhilosophers),
+		waiter:        &sync.Mutex{},
 	}
-	fmt.Printf("2");
-	forks:=make([]*sync.Mutex,numphilosophers)
-	fmt.Printf("3");
-	for i := 0; i < numphilosophers; i++ {
-		forks[i]=&sync.Mutex{}
-		fmt.Printf("4");
+	forks := make([]*sync.Mutex, numPhilosophers)
+	for i := 0; i < numPhilosophers; i++ {
+		forks[i] = &sync.Mutex{}
 	}
-	fmt.Printf("5");
-	for i := 0; i < numphilosophers; i++ {
-		table.philosophers[i]=&Philosopher{
-			id: i,
-			leftfork: forks[i],
-			rightfork: forks[(i+1)%numphilosophers],
+	for i := 0; i < numPhilosophers; i++ {
+		table.philososphers[i] = &Philosopher{
+			id:        i,
+			leftFork:  forks[i],
+			rightFork: forks[(i+1)%numPhilosophers],
 		}
-		fmt.Printf("6");
 	}
 	var wg sync.WaitGroup
-	fmt.Printf("7");
-	wg.Add(numphilosophers)
-	fmt.Printf("8");
-	for i := 0; i < numphilosophers; i++ {
-		fmt.Printf("9");
-		go func(p*Philosopher) {
+	wg.Add(numPhilosophers)
+	for i := 0; i < numPhilosophers; i++ {
+		go func(p *Philosopher) {
 			defer wg.Done()
-			fmt.Printf("!");
 			p.dine(table)
-		}(table.philosophers[i])
-		fmt.Printf("10");
+		}(table.philososphers[i])
 	}
+	wg.Wait()
 }
